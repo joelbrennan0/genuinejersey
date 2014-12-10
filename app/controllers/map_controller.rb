5a75b292dcb0b621ef68d908c4d0db518eb6d92d
Joel Brennan <joelbrennan0@gmail.com>
2014-12-10 20:09:10 +0000

    added geolaction to append marker to existing map canvas

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,7 +5,7 @@ class MapController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker| 
     marker.lat user.latitude
     marker.lng user.longitude
     marker.infowindow user.title + "<br>" + 
